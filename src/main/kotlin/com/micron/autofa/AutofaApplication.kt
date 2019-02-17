1d6391db86dc989a58e606d807097c2aa254a9bb
steve thompson <thompson4822@protonmail.com>
2019-02-17 09:09:45 -0700

    Initial commit

---
+++ src/main/kotlin/com/micron/autofa/AutofaApplication.kt
@@ -0,0 +1,11 @@
+package com.micron.autofa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutofaApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutofaApplication>(*args)
+}
---
+++ src/main/kotlin/com/micron/autofa/controllers/MainController.kt
@@ -0,0 +1,29 @@
+package com.micron.autofa.controllers
+
+import org.springframework.web.bind.annotation.*
+
+data class Greeting(val content: String)
+
+data class SearchCriteria(
+        val bySHA: Boolean,
+        val startDate: String,
+        val endDate: String,
+        val primaryKey: String,
+        val secondaryKeys: List<String>
+)
+
+data class Acknowledgement(val response: String = "I got your criteria", val criteria: SearchCriteria)
+
+
+@CrossOrigin(origins = ["http://localhost:8080"], maxAge = 3000)
+@RestController
+class MainController {
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting("Hello, $name")
+
+    @PostMapping("/search-criteria")
+    fun searchCriteria(@RequestBody criteria: SearchCriteria): Acknowledgement =
+            Acknowledgement("The Rain In Spain Falls Mainly On My Brain", criteria)
+}
