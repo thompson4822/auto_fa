e4b0645d906610d5809be3fcb3679f87284a5887
thompson4822 <thompson4822@protonmail.com>
2019-02-19 15:43:29 -0700

    Update ElasticSearchRepository.kt

--- src/main/kotlin/com/micron/autofa/repositories/ElasticSearchRepository.kt
+++ src/main/kotlin/com/micron/autofa/repositories/ElasticSearchRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository
 
 
 // No need implementation, just one interface, and you have CRUD, thanks Spring Data
-interface ElasticSearchRepository : MongoRepository<ElasticSearchEntry, Long> {
+interface ElasticSearchRepository : MongoRepository<ElasticSearchEntry, String> {
 
 /*
     fun findFirstByDomain(domain: String): Domain
@@ -19,5 +19,7 @@ interface ElasticSearchRepository : MongoRepository<ElasticSearchEntry, Long> {
     @Query("{domain: { \$regex: ?0 } })")
     fun findCustomByRegExDomain(domain: String): List<Domain>
 */
+    
+    fun findDistinctByScript(): List<String>
 
 }
